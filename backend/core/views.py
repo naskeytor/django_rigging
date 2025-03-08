be8cc5ed858966b97dbf30d57631c96c70ba4ece
Atanas Turlakov <nasko@localhost.localdomain>
2025-03-08 19:59:57 +0100

    basic configuration Django done

---
+++ backend/core/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import User, Role, Manufacturer, Size, Status, ComponentType, Model, Component, Rig, RiggingType, Rigging
+from .serializers import UserSerializer, RoleSerializer, ManufacturerSerializer, SizeSerializer, StatusSerializer, ComponentTypeSerializer, ModelSerializer, ComponentSerializer, RigSerializer, RiggingTypeSerializer, RiggingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class ManufacturerViewSet(viewsets.ModelViewSet):
+    queryset = Manufacturer.objects.all()
+    serializer_class = ManufacturerSerializer
+
+class SizeViewSet(viewsets.ModelViewSet):
+    queryset = Size.objects.all()
+    serializer_class = SizeSerializer
+
+class StatusViewSet(viewsets.ModelViewSet):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+class ComponentTypeViewSet(viewsets.ModelViewSet):
+    queryset = ComponentType.objects.all()
+    serializer_class = ComponentTypeSerializer
+
+class ModelViewSet(viewsets.ModelViewSet):
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
+
+class ComponentViewSet(viewsets.ModelViewSet):
+    queryset = Component.objects.all()
+    serializer_class = ComponentSerializer
+
+class RigViewSet(viewsets.ModelViewSet):
+    queryset = Rig.objects.all()
+    serializer_class = RigSerializer
+
+class RiggingTypeViewSet(viewsets.ModelViewSet):
+    queryset = RiggingType.objects.all()
+    serializer_class = RiggingTypeSerializer
+
+class RiggingViewSet(viewsets.ModelViewSet):
+    queryset = Rigging.objects.all()
+    serializer_class = RiggingSerializer
