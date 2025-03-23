89927ae216df12462278e83bc8bf144866f14ab5
Atanas Turlakov <nasko@localhost.localdomain>
2025-03-23 20:03:04 +0100

    Auth done and users table working

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core.models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'groups')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'groups')}
+        ),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    filter_horizontal = ('groups', 'user_permissions')
+
+admin.site.register(User, UserAdmin)
